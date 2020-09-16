1cd4016b6629001ee88de242a6106fa3f0d3425b
Artur Gunka <arturel101010@mail.ru>
2020-09-16 16:31:32 +0300

    Updates

---
+++ precache-manifest.f6eb25ba25c7a77cf27ab786fb0712ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9aa8881a4b7459510c4c76bed48482c",
+    "url": "/shoes-store/index.html"
+  },
+  {
+    "revision": "4c62862d082ac12e382d",
+    "url": "/shoes-store/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "75b039b0cb3b02c1b5f0",
+    "url": "/shoes-store/static/css/main.6c509ad7.chunk.css"
+  },
+  {
+    "revision": "4c62862d082ac12e382d",
+    "url": "/shoes-store/static/js/2.6798b45f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shoes-store/static/js/2.6798b45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b039b0cb3b02c1b5f0",
+    "url": "/shoes-store/static/js/main.ff4a978c.chunk.js"
+  },
+  {
+    "revision": "d58b4d9901f0d1e92bc9",
+    "url": "/shoes-store/static/js/runtime-main.d521dee2.js"
+  }
+]);
